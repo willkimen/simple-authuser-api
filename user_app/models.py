b90c5e51827a8f3421d2396cb72f4ce1db49a173
Willkimen <willkimen.dev@proton.me>
2024-05-03 00:29:12 -0300

    feat: Cria manager para usuário personalizado

--- user_app/models.py
+++ user_app/models.py
@@ -1,5 +1,28 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+
+
+class UserProfileManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The email address must be entered")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class UserProfile(AbstractUser):
@@ -8,11 +31,11 @@ class UserProfile(AbstractUser):
     last_name = models.CharField(max_length=100, blank=False, null=False)
     email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
 
     REQUIRED_FIELDS: list[str] = []
 
     class Meta:
-        db_table = 'user_profile'
-        verbose_name = 'User Profile'
-        verbose_name_plural = 'Users Profile'
+        db_table = "user_profile"
+        verbose_name = "User Profile"
+        verbose_name_plural = "Users Profile"
