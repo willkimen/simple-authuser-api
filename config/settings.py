74e751c2de196c7ce8c62116ebe8c1d01954b7e1
Willkimen <willkimen.dev@proton.me>
2024-05-03 00:08:06 -0300

    feat: Cria o usuário personalizado

--- config/settings.py
+++ config/settings.py
@@ -101,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'user_app.UserProfile'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class UserProfile(AbstractUser):
+    username = None
+    first_name = models.CharField(max_length=100, blank=False, null=False)
+    last_name = models.CharField(max_length=100, blank=False, null=False)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS: list[str] = []
+
+    class Meta:
+        db_table = 'user_profile'
+        verbose_name = 'User Profile'
+        verbose_name_plural = 'Users Profile'
