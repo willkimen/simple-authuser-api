b6a5932d2d2e4cae4a50f3cf769a78c0e7a46af1
Willkimen <willkimen.dev@proton.me>
2024-05-27 21:32:03 -0300

    refactor: Rename tests and perform other refactorings to UserProfile class

--- user_app/tests/tests_models/test_UserProfile.py
+++ user_app/tests/tests_models/test_UserProfile.py
@@ -12,13 +12,9 @@ def user_dict():
 
 
 @pytest.mark.django_db
-def test_create_instance_UserProfile(user_dict):
+def test_creates_user_instance(user_dict):
 
-    user_instance = UserProfile.objects.create_user(
-        email=user_dict['email'],
-        password=user_dict['password'],
-        first_name=user_dict['first_name'],
-        last_name=user_dict['last_name'])
+    user_instance = UserProfile.objects.create_user(**user_dict)
 
     assert user_instance.email == user_dict['email']
     assert user_instance.check_password(user_dict['password'])
@@ -29,57 +25,37 @@ def test_create_instance_UserProfile(user_dict):
 
 
 @pytest.mark.django_db
-def test_user_without_email_is_not_created(user_dict):
+def test_does_not_create_user_without_email(user_dict):
+    del user_dict['email']
     with pytest.raises(TypeError):
-        UserProfile.objects.create_user(
-            password=user_dict['password'],
-            first_name=user_dict['first_name'],
-            last_name=user_dict['last_name'])
+        UserProfile.objects.create_user(**user_dict)
 
 
 @pytest.mark.django_db
-def test_normalize_domain_email(user_dict):
-    user = UserProfile.objects.create_user(
-        email='email@DOMAINUPPERCASE.com',
-        password=user_dict['password'],
-        first_name=user_dict['first_name'],
-        last_name=user_dict['last_name'])
+def test_normalizes_email_domain(user_dict):
+    user_dict['email'] = 'email@DOMAINUPPERCASE.com' 
+    user = UserProfile.objects.create_user(**user_dict)
 
     assert user.email.islower()
 
 
 @pytest.mark.django_db
-def test_user_without_password_is_not_created(user_dict):
-    with pytest.raises(TypeError):
-        UserProfile.objects.create_user(
-            email=user_dict['email'],
-            first_name=user_dict['first_name'],
-            last_name=user_dict['last_name'])
-
-
-@pytest.mark.django_db
-def test_user_with_duplicate_email_is_not_created(user_dict):
+def test_does_not_create_user_with_duplicate_email(user_dict):
     with pytest.raises(IntegrityError):
-        persisted_user = UserProfile.objects.create_user(
-            email=user_dict['email'],
-            password=user_dict['password'],
-            first_name=user_dict['first_name'],
-            last_name=user_dict['last_name'])
-
-        UserProfile.objects.create_user(
-            email=persisted_user.email,
-            password=user_dict['password'],
-            first_name=user_dict['first_name'],
-            last_name=user_dict['last_name'])
+        UserProfile.objects.create_user(**user_dict)
+        UserProfile.objects.create_user(**user_dict)
 
 
 @pytest.mark.django_db
-def test_create_superuser(user_dict):
-    superuser = UserProfile.objects.create_superuser(
-            email=user_dict['email'],
-            password=user_dict['password'],
-            first_name=user_dict['first_name'],
-            last_name=user_dict['last_name'])
+def test_does_not_create_user_without_password(user_dict):
+    del user_dict['password']
+    with pytest.raises(TypeError):
+        UserProfile.objects.create_user(**user_dict)
+
+
+@pytest.mark.django_db
+def test_creates_superuser(user_dict):
+    superuser = UserProfile.objects.create_superuser(**user_dict)
 
     assert superuser.is_staff
     assert superuser.is_superuser
