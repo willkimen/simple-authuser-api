0e65db95878781efb35ca076f0e8d0bf6502dc69
Willkimen <willkimen.dev@proton.me>
2024-05-29 14:42:30 -0300

    feat: Add route and view for user registration

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import register
 
 urlpatterns = [
-    
+    path('users/', register, name='register'),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from user_app.serializers import UserRegistrationSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+
+@api_view(['POST'])
+def register(request):
+    serializer = UserRegistrationSerializer(data=request.data)
+    if not serializer.is_valid():
+        return Response({'validation_errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = serializer.save()
+    return Response({'user_id': user.id, 'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
