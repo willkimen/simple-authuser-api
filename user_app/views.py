f7d72e22b9792d2dad58fede879061d9c0a8d9ff
Willkimen <willkimen.dev@proton.me>
2024-06-06 19:15:07 -0300

    feat: Add user update view and reformat code in user_app/views.py
    - Add PATCH /update endpoint for updating user details.
    - Use UserUpdateSerializer for partial updates.
    - Reformat existing code for consistency and readability.

--- user_app/views.py
+++ user_app/views.py
@@ -1,14 +1,45 @@
-from rest_framework.response import Response
-from user_app.serializers import UserRegistrationSerializer
-from rest_framework.decorators import api_view
+from django.contrib.auth import get_user_model
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from user_app.serializers import UserSerializer, UserUpdateSerializer
+
+User = get_user_model()
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def register(request):
-    serializer = UserRegistrationSerializer(data=request.data)
+    serializer = UserSerializer(data=request.data)
     if not serializer.is_valid():
-        return Response({'validation_errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"validation_errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )
 
     user = serializer.save()
-    return Response({'user_id': user.id, 'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+    return Response(
+        {"user_id": user.id, "message": "User registered successfully"},
+        status=status.HTTP_201_CREATED,
+    )
+
+
+@api_view(["PATCH"])
+def update(request, id: int):
+    try:
+        user = User.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return Response(
+            {"message": "User with id not found."}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    serializer = UserUpdateSerializer(instance=user, data=request.data, partial=True)
+    if not serializer.is_valid():
+        return Response(
+            {"validation_errors": serializer.errors},
+            status=status.status.HTTP_400_BAD_REQUEST,
+        )
+
+    serializer.save()
+    return Response(
+        {"message": "User updated successfully."}, status=status.HTTP_200_OK
+    )
