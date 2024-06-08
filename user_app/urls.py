617c2d22a7b2c1f6cfa1a7841d8278c57a630a9b
Willkimen <willkimen.dev@proton.me>
2024-06-08 17:07:32 -0300

    feat: Add user update route and reformat URLs
    - Add PATCH /users/<int:id>/ route for updating user details.
    - Reformat existing URL patterns for consistency.

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import register
+
+from .views import register, update
 
 urlpatterns = [
-    path('users/', register, name='register'),
+    path("users/", register, name="register"),
+    path("users/<int:id>/", update, name="update"),
 ]
