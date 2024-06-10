a6696902a4558fa5121775cf583227cb5fe83816
Willkimen <willkimen.dev@proton.me>
2024-06-10 14:54:00 -0300

    test: Refactor UserProfile model tests with improved naming and docstrings
    - Rename user_dict fixture to user_data.
    - Add docstrings to tests for better clarity.
    - Improve assertion error messages for better debugging.

--- user_app/tests/tests_models/test_UserProfile.py
+++ user_app/tests/tests_models/test_UserProfile.py
@@ -1,61 +1,114 @@
 import pytest
 from django.db import IntegrityError
+
 from user_app.models import UserProfile
 
 
 @pytest.fixture
-def user_dict():
-    return {'email': 'user@email.com',
-            'password': '1234',
-            'first_name': 'my_name',
-            'last_name': 'my_last_name'}
+def user_data():
+    """
+    Retorna dados de usuário padrão para uso nos testes.
+
+    Returns:
+        dict: Dados do usuário.
+    """
+
+    return {
+        "email": "user@email.com",
+        "password": "1234",
+        "first_name": "my_name",
+        "last_name": "my_last_name",
+    }
 
 
 @pytest.mark.django_db
-def test_creates_user_instance(user_dict):
+def test_creates_user_instance(user_data):
+    """
+    Testa se uma instância de usuário é criada corretamente com os dados fornecidos.
 
-    user_instance = UserProfile.objects.create_user(**user_dict)
+    Args:
+        user_data (dict): Dados do usuário.
+    """
+    user_instance = UserProfile.objects.create_user(**user_data)
 
-    assert user_instance.email == user_dict['email']
-    assert user_instance.check_password(user_dict['password'])
-    assert user_instance.first_name == user_dict['first_name']
-    assert user_instance.last_name == user_dict['last_name']
-    assert not user_instance.is_staff
-    assert not user_instance.is_superuser
+    assert user_instance.email == user_data["email"], "Email do usuário não coincide."
+    assert user_instance.check_password(
+        user_data["password"]
+    ), "A senha do usuário não coincide."
+    assert (
+        user_instance.first_name == user_data["first_name"]
+    ), "O primeiro nome do usuário não coincide."
+    assert (
+        user_instance.last_name == user_data["last_name"]
+    ), "O sobrenome do usuário não coincide."
+    assert not user_instance.is_staff, "Usuário não deve ser staff."
+    assert not user_instance.is_superuser, "Usuário não deve ser superusuário."
 
 
 @pytest.mark.django_db
-def test_does_not_create_user_without_email(user_dict):
-    del user_dict['email']
+def test_does_not_create_user_without_email(user_data):
+    """
+    Testa se um erro é levantado ao tentar criar um usuário sem email.
+
+    Args:
+        user_data (dict): Dados do usuário.
+    """
+    del user_data["email"]
     with pytest.raises(TypeError):
-        UserProfile.objects.create_user(**user_dict)
+        UserProfile.objects.create_user(**user_data)
 
 
 @pytest.mark.django_db
-def test_normalizes_email_domain(user_dict):
-    user_dict['email'] = 'email@DOMAINUPPERCASE.com' 
-    user = UserProfile.objects.create_user(**user_dict)
+def test_normalizes_email_domain(user_data):
+    """
+    Testa se o domínio do email é normalizado para minúsculas.
 
-    assert user.email.islower()
+    Args:
+        user_data (dict): Dados do usuário.
+    """
+    user_data["email"] = "email@DOMAINUPPERCASE.com"
+    user = UserProfile.objects.create_user(**user_data)
+
+    assert (
+        user.email.islower()
+    ), "O domínio do email não foi normalizado para minúsculas."
 
 
 @pytest.mark.django_db
-def test_does_not_create_user_with_duplicate_email(user_dict):
+def test_does_not_create_user_with_duplicate_email(user_data):
+    """
+    Testa se um erro de integridade é levantado ao tentar criar dois usuários com o mesmo email.
+
+    Args:
+        user_data (dict): Dados do usuário.
+    """
     with pytest.raises(IntegrityError):
-        UserProfile.objects.create_user(**user_dict)
-        UserProfile.objects.create_user(**user_dict)
+        UserProfile.objects.create_user(**user_data)
+        UserProfile.objects.create_user(**user_data)
 
 
 @pytest.mark.django_db
-def test_does_not_create_user_without_password(user_dict):
-    del user_dict['password']
+def test_does_not_create_user_without_password(user_data):
+    """
+    Testa se um erro é levantado ao tentar criar um usuário sem senha.
+
+    Args:
+        user_data (dict): Dados do usuário.
+    """
+    del user_data["password"]
     with pytest.raises(TypeError):
-        UserProfile.objects.create_user(**user_dict)
+        UserProfile.objects.create_user(**user_data)
 
 
 @pytest.mark.django_db
-def test_creates_superuser(user_dict):
-    superuser = UserProfile.objects.create_superuser(**user_dict)
+def test_creates_superuser(user_data):
+    """
+    Testa se uma instância de superusuário é criada corretamente com os dados fornecidos.
 
-    assert superuser.is_staff
-    assert superuser.is_superuser
+    Args:
+        user_data (dict): Dados do usuário.
+    """
+    superuser = UserProfile.objects.create_superuser(**user_data)
+
+    assert superuser.is_staff, "Superusuário deve ser staff."
+    assert superuser.is_superuser, "Superusuário deve ser superusuário."
