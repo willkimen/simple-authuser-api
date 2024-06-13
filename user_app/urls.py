15ef9e3c09356459fe7f725ea1b97d1c20da4491
Willkimen <willkimen.dev@proton.me>
2024-06-13 17:48:42 -0300

    feat: Add url for registration confirmation

--- user_app/urls.py
+++ user_app/urls.py
@@ -5,4 +5,9 @@ from .views import register, update
 urlpatterns = [
     path("users/", register, name="register"),
     path("users/<int:id>/", update, name="update"),
+    path(
+        "users/confirmation_register/<int:id>/<str:token>/",
+        confirmation_register,
+        name="confirmation_register",
+    ),
 ]
