3b02c750197003850eb9d156c0d4602120bdd054
Willkimen <willkimen.dev@proton.me>
2024-06-15 21:55:31 -0300

    docs: Updates the email_service.py module documentation for English

--- user_app/utils/email_service.py
+++ user_app/utils/email_service.py
@@ -13,13 +13,13 @@ from ..utils.token import user_active_generate_token
 
 class EmailThread(threading.Thread):
     """
-    Classe para enviar email em uma nova thread.
+    Class to send email in a new thread.
 
     Args:
-        email (EmailMessage): A instância do email a ser enviado.
+        email (EmailMessage): The email instance to be sent.
 
-    Métodos:
-        run(): Envia o email.
+    Methods:
+        run(): Sends the email.
     """
 
     def __init__(self, email: EmailMessage) -> None:
@@ -28,34 +28,34 @@ class EmailThread(threading.Thread):
 
     def run(self) -> None:
         """
-        Envia o email quando a thread é iniciada.
+        Sends the email when the thread is started.
         """
         self.email.send()
 
 
 def send_activation_email(user: User, request: HttpRequest) -> None:
     """
-    Envia um email de ativação para o usuário.
+    Sends an activation email to the user.
 
     Args:
-        user (User): A instância do usuário para quem o email será enviado.
-        request (HttpRequest): A instância do request atual para obter o site atual.
+        user (User): The user instance to whom the email will be sent.
+        request (HttpRequest): The current request instance to get the current site.
 
-    Esta função cria o conteúdo do email de ativação da conta,incluindo um link de ativação com um token seguro.
-    O email é então enviado em uma nova thread para evitar bloquear o processo principal.
+    This function creates the content of the account activation email, including an activation link with a secure token.
+    The email is then sent in a new thread to avoid blocking the main process.
     """
-    current_site = get_current_site(request)  # Obtém o site atual a partir do request
-    email_subject = "Activate your account"  # Assunto do email
+    current_site = get_current_site(request)  # Get the current site from the request
+    email_subject = "Activate your account"  # Email subject
     uid = urlsafe_base64_encode(
         force_bytes(user.id)
-    )  # Codifica o ID do usuário em base64
+    )  # Encode the user ID in base64
     token = user_active_generate_token.make_token(
         user
-    )  # Gera um token de ativação para o usuário
+    )  # Generate an activation token for the user
     end_point = reverse("confirmation_register", kwargs={"id": uid, "token": token})
 
     activation_link = (
-        f"http://{current_site.domain}/api/v1/{end_point}"  # Cria o link de ativação
+        f"http://{current_site.domain}/api/v1/{end_point}"  # Create the activation link
     )
 
     email_body = f"""
@@ -70,6 +70,6 @@ def send_activation_email(user: User, request: HttpRequest) -> None:
 
     email = EmailMessage(
         subject=email_subject, body=email_body, to=[user.email]
-    )  # Cria a mensagem de email
-    email_thread = EmailThread(email)  # Cria uma nova thread para enviar o email
-    email_thread.start()  # Inicia a thread para enviar o email
+    )  # Create the email message
+    email_thread = EmailThread(email)  # Create a new thread to send the email
+    email_thread.start()  # Start the thread to send the email
