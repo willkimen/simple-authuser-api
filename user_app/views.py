bc882ef80384b2e8cb6cf5053161a199c8e168f1
Willkimen <willkimen.dev@proton.me>
2024-06-29 00:19:00 -0300

    feat: Add verification email view

--- user_app/views.py
+++ user_app/views.py
@@ -1,6 +1,10 @@
+import os
 import smtplib
 
 from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -8,6 +12,7 @@ from rest_framework.response import Response
 from user_app.serializers import UserSerializer
 
 from .utils.email_service import send_activation_email
+from .utils.token import user_active_generate_token
 
 User = get_user_model()
 
@@ -83,5 +88,40 @@ def update(request, id: int):
     )
 
 
-# TODO nao finalizado
-def confirmation_register(request, id, token): ...
+@api_view(["GET"])
+def confirmation_register(request, id, token):
+    """
+    Confirms user registration and activates the account.
+
+    Args:
+        request (Request): The request instance.
+        id (str): The encoded user ID.
+        token (str): The user's activation token.
+
+    Returns:
+        HttpResponseRedirect: Redirects to the success or failure URL depending on token validation.
+
+    This function is called when a user clicks on the activation link sent via email.
+    It decodes the user ID, checks the existence of the user, and validates the token.
+    If the token is valid, the user's account is activated and they are redirected to the success URL.
+    Otherwise, the user is redirected to the failure URL.
+    """
+    # Decode the user ID from the URL-safe base64 string
+    id = int(force_str(urlsafe_base64_decode(id)))
+
+    try:
+        # Attempt to retrieve the user by ID
+        user = User.objects.get(id=id)
+    except User.DoesNotExist:
+        # Redirect to the failure URL if the user does not exist
+        return redirect(os.environ.get("ENV_EMAIL_CONFIRM_FAILURE_URL"))
+
+    # Check if the token is valid
+    if user_active_generate_token.check_token(user, token):
+        user.is_active = True  # Activate the user's account
+        user.save()
+        # Redirect to the success URL
+        return redirect(os.environ.get("ENV_EMAIL_CONFIRM_SUCCESS_URL"))
+    else:
+        # Redirect to the failure URL if the token is invalid
+        return redirect(os.environ.get("ENV_EMAIL_CONFIRM_FAILURE_URL"))
