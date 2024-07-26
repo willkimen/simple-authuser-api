e3282551e59977980661be5151c9159751c763e6
Willkimen <willkimen.dev@proton.me>
2024-07-26 00:13:39 -0300

    docs: Added documentation for the functions in jwt_token.py

--- user_app/utils/jwt_token.py
+++ user_app/utils/jwt_token.py
@@ -8,17 +8,11 @@ import jwt
 from user_app.exceptions import JWTBlackListException
 from user_app.models import JWTBlackList
 
-"""
-
-No login, o client recebe um access e um refresh.
-Com o refresh, o client vai pegando novos access, sempre que o access expirar.
-O client vai poder pegar novos access ate que o refresh expirar, quando expirar, tera que fazer login novamente, o que ira gerar um novo par de access e refresh.
-
-Processo de logout ira revogar tanto o access quanto refresh.
-"""
-
 
 def __create_jti(user_id: int) -> str:
+    """
+    Create a unique JWT ID (JTI) using the user ID and the current timestamp.
+    """
     unix_timestamp = str(int(time.time()))
     hash = str(user_id) + unix_timestamp
     jti = hashlib.sha256(hash.encode()).hexdigest()
@@ -27,6 +21,16 @@ def __create_jti(user_id: int) -> str:
 
 
 def __create_exp(is_refresh: bool = False) -> int:
+    """
+    Create an expiration timestamp for a token.
+
+    Args:
+        is_refresh (bool): Indicates whether the expiration is for a refresh token.
+                           If True, sets expiration to one week; otherwise, sets to 10 minutes.
+
+    Returns:
+        int: The expiration timestamp.
+    """
     if is_refresh is True:
         exp_date = datetime.now() + timedelta(weeks=1)
         return int(exp_date.timestamp())
@@ -36,6 +40,17 @@ def __create_exp(is_refresh: bool = False) -> int:
 
 
 def __create_payload(user_id: int, is_refresh: bool = False) -> dict:
+    """
+    Create a JWT payload for a user.
+
+    Args:
+        user_id (int): The ID of the user.
+        is_refresh (bool): Indicates whether the payload is for a refresh token.
+                           If True, creates a refresh token payload; otherwise, creates an access token payload.
+
+    Returns:
+        dict: The generated JWT payload.
+    """
     if is_refresh:
         return {
             "uid": user_id,
@@ -53,16 +68,34 @@ def __create_payload(user_id: int, is_refresh: bool = False) -> dict:
 
 
 def __create_refresh_jwt(user_id: int) -> str:
+    """
+    Create a refresh JWT for a user.
+
+    Returns:
+        str: The encoded refresh JWT.
+    """
     return jwt.encode(
         __create_payload(user_id, is_refresh=True), os.environ.get("ENV_JWT_SECRET")
     )
 
 
 def create_access_jwt(user_id: int) -> str:
+    """
+    Create a access JWT for a user.
+
+    Returns:
+        str: The encoded access JWT.
+    """
     return jwt.encode(__create_payload(user_id), os.environ.get("ENV_JWT_SECRET"))
 
 
 def create_pair_jwt(user_id: int) -> dict:
+    """
+    Create a pair of JWTs (access and refresh) for a user.
+
+    Returns:
+        dict: A dictionary containing the access and refresh JWTs.
+    """
     return {
         "access": create_access_jwt(user_id),
         "refresh": __create_refresh_jwt(user_id),
@@ -70,6 +103,24 @@ def create_pair_jwt(user_id: int) -> dict:
 
 
 def check_token(token: str) -> dict:
+    """
+    Check the validity of a JWT.
+
+    Args:
+        token (str): The JWT to be checked.
+
+    Returns:
+        dict: The decoded JWT payload if the token is valid.
+
+    Raises:
+        jwt.exceptions.ExpiredSignatureError: If the token has expired.
+        jwt.exceptions.InvalidAlgorithmError: If the token's algorithm is invalid.
+        jwt.exceptions.MissingRequiredClaimError: If the token is missing required claims.
+        jwt.exceptions.InvalidSignatureError: If the token signature is invalid.
+        jwt.exceptions.DecodeError: If the token cannot be decoded.
+        jwt.exceptions.InvalidTokenError: If the token is invalid.
+        JWTBlackListException: If the token is found in the blacklist.
+    """
     try:
         payload = jwt.decode(
             jwt=token, key=os.environ.get("ENV_JWT_SECRET"), algorithms=["HS256"]
