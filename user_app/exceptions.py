5d727cfe20df378aeebceb39f9ebe33caa024286
Willkimen <willkimen.dev@proton.me>
2024-08-05 20:42:46 -0300

    feat: Create constant with default message for JWT in blacklist.

--- user_app/exceptions.py
+++ user_app/exceptions.py
@@ -1,5 +1,8 @@
+from user_app.constants import jwt_error_messages
+
+
 class JWTBlackListException(Exception):
     def __init__(self, message=None):
         if message is None:
-            message = "JWT is blacklisted and therefore invalid"
+            message = jwt_error_messages.JWT_IN_BLACKLIST
         super().__init__(message)
