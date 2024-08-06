91060b27da88ec6ebf95905370b5bed674868c85
Willkimen <willkimen.dev@proton.me>
2024-08-06 17:09:09 -0300

    tests: Add tests for JwtAuthentication class

---
+++ user_app/tests/tests_authentication_classes/test_jwt_authentication.py
@@ -0,0 +1,420 @@
+"""
+This module contains tests for the `authenticate()` method of the `JWTAuthentication` class.
+
+The tests cover various scenarios related to JWT authentication, including:
+- Empty or malformed authorization headers
+- Expired JWTs or JWTs with an invalid secret
+- Malformed JWTs or JWTs with invalid algorithms
+- JWTs that are blacklisted
+- JWTs for nonexistent users
+- Successful authentication with a valid JWT
+
+It uses the `pytest` and `unittest.mock` libraries.
+"""
+
+import base64
+import json
+from datetime import datetime, timedelta
+from unittest.mock import patch
+
+import jwt
+import pytest
+from django.contrib.auth import get_user_model
+from django.test import RequestFactory
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.request import Request
+
+from user_app.authentication_classes import JWTAuthentication
+from user_app.constants import (
+    authentication_error_messages,
+    jwt_error_messages,
+    response_messages,
+)
+from user_app.models import JWTBlackList
+
+# ============== Objects and constants ===============
+User = get_user_model()
+factory = RequestFactory()
+jwt_authentication = JWTAuthentication()
+FAKE_SECRET = "fake_secret"
+FAKE_JTI_IN_BLACKLIST = "fake_jti_in_blacklist"
+FAKE_UID_EXIST_IN_DATABASE = 1
+FAKE_UID = 10
+FAKE_PAYLOAD = {
+    "uid": FAKE_UID,
+    "typ": "fake_typ",
+    "jti": "fake_jti",
+    "exp": int((datetime.now() + timedelta(days=1)).timestamp()),
+}
+
+
+# ================== Fixtures ===================
+@pytest.fixture
+def empty_auth_header_request() -> Request:
+    """
+    Create a request object with an empty Authorization header.
+
+    Returns:
+        Request: A request object with an empty Authorization header.
+    """
+    return Request(factory.get("/", HTTP_AUTHORIZATION=""))
+
+
+@pytest.fixture
+def incorrect_auth_header_request() -> Request:
+    """
+    Create a request object with an incorrect Authorization header.
+
+    Returns:
+        Request: A request object with an incorrect Authorization header.
+    """
+    return Request(factory.get("/", HTTP_AUTHORIZATION="Type_Incorret"))
+
+
+@pytest.fixture
+def incorrect_format_auth_header_request() -> list[Request]:
+    """
+    Create a list of request objects with incorrectly formatted Authorization headers.
+
+    Returns:
+        list[Request]: A list of request objects with incorrect Authorization headers.
+    """
+    return [
+        Request(factory.get("/", HTTP_AUTHORIZATION="Bearer")),
+        Request(factory.get("/", HTTP_AUTHORIZATION="Bearer jwt_fake extra")),
+    ]
+
+
+@pytest.fixture
+def jwt_request_with_nonexistent_user() -> Request:
+    """
+    Create a request object with a JWT token for a nonexistent user.
+
+    Returns:
+        Request: A request object with a JWT token in the Authorization header.
+    """
+    token = jwt.encode(FAKE_PAYLOAD, FAKE_SECRET)
+    return Request(factory.get("/", HTTP_AUTHORIZATION=f"Bearer {token}"))
+
+
+@pytest.fixture
+def expired_jwt_request() -> Request:
+    """
+    Create a request object with an expired JWT token.
+
+    Returns:
+        Request: A request object with an expired JWT token in the Authorization header.
+    """
+    # Create a expired date
+    exp_expired = int((datetime.now() - timedelta(seconds=10)).timestamp())
+    FAKE_PAYLOAD["exp"] = exp_expired
+
+    token = jwt.encode(FAKE_PAYLOAD, FAKE_SECRET)
+
+    return Request(factory.get("/", HTTP_AUTHORIZATION=f"Bearer {token}"))
+
+
+@pytest.fixture
+def jwt_request_with_invalid_secret() -> Request:
+    """
+    Create a request object with a JWT token signed with an incorrect secret.
+
+    Returns:
+        Request: A request object with a JWT token signed with an incorrect secret in the Authorization header.
+    """
+    token = jwt.encode(FAKE_PAYLOAD, "invalid_secret")
+
+    return Request(factory.get("/", HTTP_AUTHORIZATION=f"Bearer {token}"))
+
+
+@pytest.fixture
+def malformed_jwt_request() -> Request:
+    """
+    Create a request object with a malformed JWT token.
+
+    Returns:
+        Request: A request object with a malformed JWT token in the Authorization header.
+    """
+    return Request(factory.get("/", HTTP_AUTHORIZATION="Bearer malformed.token.string"))
+
+
+@pytest.fixture
+def jwt_request_with_invalid_algorithm() -> Request:
+    """
+    Create a request object with a JWT token that has an invalid algorithm.
+
+    Returns:
+        Request: A request object with a JWT token that specifies an invalid algorithm in the header.
+    """
+    # Encode the payload into a JWT token with the correct secret
+    token = jwt.encode(FAKE_PAYLOAD, FAKE_SECRET)
+
+    # Split the token into its components: header, payload, and signature
+    header, payload, signature = token.split(".")
+
+    # Decode the header from Base64 URL safe format
+    header_data = json.loads(base64.urlsafe_b64decode(header + "==").decode("utf-8"))
+
+    # Modify the algorithm in the header to an invalid one (RS256)
+    header_data["alg"] = "RS256"  # Invalid algorithm
+
+    # Re-encode the modified header into Base64 URL safe format
+    modified_header = (
+        base64.urlsafe_b64encode(json.dumps(header_data).encode("utf-8"))
+        .decode("utf-8")
+        .rstrip("=")
+    )
+
+    # Reassemble the token with the modified header
+    token_with_invalid_algorithm = f"{modified_header}.{payload}.{signature}"
+
+    # Create and return a request object with the JWT token that has an invalid algorithm
+    return Request(
+        factory.get("/", HTTP_AUTHORIZATION=f"Bearer {token_with_invalid_algorithm}")
+    )
+
+
+@pytest.fixture
+def request_with_blacklisted_jwt() -> Request:
+    """
+    Create a request object with a JWT token that is blacklisted.
+
+    Returns:
+        Request: A request object with a JWT token that has a JTI in the blacklist.
+    """
+    # Set the JTI of the payload to a value that is known to be in the blacklist
+    FAKE_PAYLOAD["jti"] = FAKE_JTI_IN_BLACKLIST
+
+    token = jwt.encode(FAKE_PAYLOAD, FAKE_SECRET)
+
+    return Request(factory.get("/", HTTP_AUTHORIZATION=f"Bearer {token}"))
+
+
+@pytest.fixture
+def valid_jwt_request_and_token() -> list:
+    """
+    Create a JWT token and a request object with the valid token.
+
+    Returns:
+        list: A list containing:
+            - A valid JWT token.
+            - A request object with the valid JWT token in the Authorization header.
+    """
+
+    # set id of a user that exists
+    FAKE_PAYLOAD["uid"] = FAKE_UID_EXIST_IN_DATABASE
+    token = jwt.encode(FAKE_PAYLOAD, FAKE_SECRET)
+
+    request_and_token = [
+        token,
+        Request(factory.get("/", HTTP_AUTHORIZATION=f"Bearer {token}")),
+    ]
+
+    return request_and_token
+
+
+# ================ Tests =======================
+def test_authentication_fails_when_empty_auth_header(
+    empty_auth_header_request: Request,
+):
+    """
+    Test that authentication fails when the Authorization header is empty.
+
+    Args:
+        empty_auth_header_request (Request): A request object with an empty Authorization header.
+    """
+    expected_error_message = authentication_error_messages.AUTHORIZATION_HEADER_MISSING
+    with pytest.raises(AuthenticationFailed) as e:
+        jwt_authentication.authenticate(empty_auth_header_request)
+
+    assert expected_error_message == str(e.value)
+
+
+def test_authentication_fails_when_incorrect_type_auth_header(
+    incorrect_auth_header_request: Request,
+):
+    """
+    Test that authentication fails when the Authorization header has an incorrect type.
+
+    Args:
+        incorrect_auth_header_request (Request): A request object with an incorrect Authorization header type.
+    """
+    expected_error_message = (
+        authentication_error_messages.AUTHORIZATION_HEADER_WITHOUT_BEARER
+    )
+    with pytest.raises(AuthenticationFailed) as e:
+        jwt_authentication.authenticate(incorrect_auth_header_request)
+
+    assert expected_error_message == str(e.value)
+
+
+def test_authentication_fails_when_incorrect_format_auth_header(
+    incorrect_format_auth_header_request: list[Request],
+):
+    """
+    Test that authentication fails when the Authorization header is in an incorrect format.
+
+    Args:
+        incorrect_format_auth_header_request (list[Request]): A list of request objects with incorrectly formatted Authorization headers.
+    """
+    expected_error_message = (
+        authentication_error_messages.INVALID_AUTHORIZATION_HEADER_FORMAT
+    )
+
+    for request in incorrect_format_auth_header_request:
+        with pytest.raises(AuthenticationFailed) as e:
+            jwt_authentication.authenticate(request)
+
+        assert expected_error_message == str(e.value)
+
+
+@patch("user_app.utils.jwt_token.os.environ.get", return_value=FAKE_SECRET)
+def test_authentication_fails_when_expired_jwt(
+    mock_jwt_secret,
+    expired_jwt_request: Request,
+):
+    """
+    Test that authentication fails when the JWT is expired.
+
+    Args:
+        mock_jwt_secret (MagicMock): Mocked environment variable for JWT secret.
+        expired_jwt_request (Request): Request object with an expired JWT in the Authorization header.
+    """
+    expected_error_message = jwt_error_messages.EXPIRED_SIGNATURE
+    with pytest.raises(AuthenticationFailed) as e:
+        jwt_authentication.authenticate(expired_jwt_request)
+
+    assert expected_error_message == str(e.value)
+
+
+@patch("user_app.utils.jwt_token.os.environ.get", return_value=FAKE_SECRET)
+def test_authentication_fails_when_invalid_secret_jwt(
+    mock_jwt_secret,
+    jwt_request_with_invalid_secret: Request,
+):
+    """
+    Test that authentication fails when the JWT has an invalid secret.
+
+    Args:
+        mock_jwt_secret (MagicMock): Mocked environment variable for JWT secret.
+        jwt_request_with_invalid_secret (Request): Request object with a JWT encoded using an incorrect secret.
+    """
+    expected_error_message = jwt_error_messages.INVALID_SIGNATURE
+    with pytest.raises(AuthenticationFailed) as e:
+        jwt_authentication.authenticate(jwt_request_with_invalid_secret)
+
+    assert expected_error_message == str(e.value)
+
+
+@patch("user_app.utils.jwt_token.os.environ.get", return_value=FAKE_SECRET)
+def test_authentication_fails_when_malformed_jwt(
+    mock_jwt_secret,
+    malformed_jwt_request: Request,
+):
+    """
+    Test that authentication fails when the JWT is malformed.
+
+    Args:
+        mock_jwt_secret (MagicMock): Mocked environment variable for JWT secret.
+        malformed_jwt_request (Request): Request object with a malformed JWT.
+    """
+    expected_error_message = jwt_error_messages.DECODE_ERROR
+    with pytest.raises(AuthenticationFailed) as e:
+        jwt_authentication.authenticate(malformed_jwt_request)
+
+    assert expected_error_message == str(e.value)
+
+
+@patch("user_app.utils.jwt_token.os.environ.get", return_value=FAKE_SECRET)
+def test_authentication_fails_when_blacklisted_jwt(
+    mock_jwt_secret,
+    jwt_request_with_invalid_algorithm: Request,
+):
+    expected_error_message = jwt_error_messages.INVALID_ALGORITHM
+    with pytest.raises(AuthenticationFailed) as e:
+        jwt_authentication.authenticate(jwt_request_with_invalid_algorithm)
+
+    assert expected_error_message == str(e.value)
+
+
+@pytest.mark.django_db
+@patch("user_app.utils.jwt_token.os.environ.get", return_value=FAKE_SECRET)
+def test_authentication_fails_when_blacklisted_jwt(
+    mock_jwt_secret,
+    request_with_blacklisted_jwt: Request,
+):
+    """
+    Test that authentication fails when the JWT is blacklisted.
+
+    Args:
+        mock_jwt_secret (MagicMock): Mocked environment variable for JWT secret.
+        request_with_blacklisted_jwt (Request): Request object with a blacklisted JWT.
+    """
+    JWTBlackList.objects.create(
+        jti=FAKE_JTI_IN_BLACKLIST,
+        exp=FAKE_PAYLOAD["exp"],
+        typ=FAKE_PAYLOAD["typ"],
+    )
+    expected_error_message = jwt_error_messages.JWT_IN_BLACKLIST
+    with pytest.raises(AuthenticationFailed) as e:
+        jwt_authentication.authenticate(request_with_blacklisted_jwt)
+
+    assert expected_error_message == str(e.value)
+
+
+@pytest.mark.django_db
+@patch("user_app.utils.jwt_token.os.environ.get", return_value=FAKE_SECRET)
+def test_authentication_fails_when_nonexistent_user(
+    mock_jwt_secret,
+    jwt_request_with_nonexistent_user: Request,
+):
+    """
+    Test that authentication fails when the JWT is for a nonexistent user.
+
+    Args:
+        mock_jwt_secret (MagicMock): Mocked environment variable for JWT secret.
+        jwt_request_with_nonexistent_user (Request): Request object with a JWT for a nonexistent user.
+    """
+    expected_error_message = response_messages.USER_NOT_FOUND
+    with pytest.raises(AuthenticationFailed) as e:
+        jwt_authentication.authenticate(jwt_request_with_nonexistent_user)
+
+    assert expected_error_message == str(e.value)
+
+
+@pytest.mark.django_db
+@patch("user_app.utils.jwt_token.os.environ.get", return_value=FAKE_SECRET)
+def test_authentication_success(
+    mock_jwt_secret,
+    valid_jwt_request_and_token: list,
+):
+    """
+    Test that authentication succeeds with a valid JWT.
+
+    Args:
+        mock_jwt_secret (MagicMock): Mocked environment variable for JWT secret.
+        valid_jwt_request_and_token (list): List containing a valid JWT and corresponding request object.
+
+    Asserts:
+        User: The user object returned by authentication matches the expected user.
+        str: The JWT returned by authentication matches the expected JWT.
+    """
+    user_expected = User.objects.create_user(
+        id=FAKE_UID_EXIST_IN_DATABASE,
+        first_name="John",
+        last_name="Doe",
+        email="johndoe@email.com",
+        password="1234",
+        is_active=True,
+    )
+    token_expected, request = valid_jwt_request_and_token
+
+    user, token = jwt_authentication.authenticate(request)
+
+    assert user_expected.id == user.id
+    assert user_expected.first_name == user.first_name
+    assert user_expected.last_name == user.last_name
+    assert user_expected.email == user.email
+    assert user_expected.password == user.password
+    assert user_expected.is_active == user.is_active
+    assert token_expected == token
