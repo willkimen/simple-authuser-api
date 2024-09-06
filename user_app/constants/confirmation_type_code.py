469143483e30ec4626c51cbe99bbacb331c2b0c0
Willkimen <willkimen.dev@proton.me>
2024-09-06 15:47:17 -0300

    chore: Remove confirmation_type_code.py module due to its obsolescence after changes in account activation logic

--- user_app/constants/confirmation_type_code.py
+++
@@ -1,9 +0,0 @@
-"""
-Constants related to the ConfirmationCode model.
-This module provides names of confirmation code types for user account activation, email change, password change, and password reset.
-"""
-
-ACCOUNT_ACTIVATION = "account_activation"
-EMAIL_CHANGE = "email_change"
-PASSWORD_CHANGE = "password_change"
-PASSWORD_RESET = "password_reset"
