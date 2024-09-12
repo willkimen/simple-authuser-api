d6f433c20ec163208d9bcbbc91ad779ae3fa9d16
Willkimen <willkimen.dev@proton.me>
2024-09-12 10:55:17 -0300

    tests: Creates tests for send_code_to_email_change() view.

--- user_app/constants/path_for_mock.py
+++ user_app/constants/path_for_mock.py
@@ -7,6 +7,11 @@ activate_account_view_path = importlib.import_module(
 ).__name__
 
 crud_view_path = importlib.import_module("user_app.views.crud_view").__name__
+
+change_email_view_path = importlib.import_module(
+    "user_app.views.change_email_view"
+).__name__
+
 email_service_module_path = importlib.import_module(
     "user_app.utils.email_service"
 ).__name__
