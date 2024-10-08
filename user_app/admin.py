dd814680d59600b55ea6d5480866fdbf384dd920
Willkimen <willkimen.dev@proton.me>
2024-10-08 14:24:48 -0300

    feat: Create custom admin classes for Django models.

--- user_app/admin.py
+++ user_app/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from user_app import models
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+
+    ordering = ("id",)
+    list_per_page = 10
+
+
+@admin.register(models.ValidTokenModel)
+class ValidTokenAdmin(admin.ModelAdmin):
+    list_display = ("id", "jti", "exp", "typ", "user_id")
+
+    ordering = ("id",)
+    list_per_page = 10
+
+
+@admin.register(models.BlacklistTokenModel)
+class BlacklistTokenAdmin(admin.ModelAdmin):
+    list_display = ("id", "jti", "exp", "typ", "user_id")
+
+    ordering = ("id",)
+    list_per_page = 10
+
+
+@admin.register(models.AccountActivationCodeModel)
+class AccountActivationCodeAdmin(admin.ModelAdmin):
+    list_display = ("id", "code", "created_at", "expires_at", "user_id")
+
+    ordering = ("id",)
+    list_per_page = 10
+
+
+@admin.register(models.ChangeEmailCodeModel)
+class ChangeEmailCodeCodeAdmin(admin.ModelAdmin):
+    list_display = ("id", "code", "created_at", "expires_at", "user_id", "new_email")
+
+    ordering = ("id",)
+    list_per_page = 10
+
+
+@admin.register(models.ResetPasswordCodeModel)
+class ResetPasswordCodeAdmin(admin.ModelAdmin):
+    list_display = ("id", "code", "created_at", "expires_at", "user_id")
+
+    ordering = ("id",)
+    list_per_page = 10
