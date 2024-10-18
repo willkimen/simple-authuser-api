325cd941686251ed8919dbc45c48cc2d441e91c3
Willkimen <willkimen.dev@proton.me>
2024-10-18 18:19:31 -0300

    chore: Move env.example files to a dedicated directory for better organization.

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -11,7 +11,7 @@ PRODUCTION = "production"
 base_path = Path(__file__).resolve().parent.parent.parent
 
 # Load environment variables from the .env.base file
-load_dotenv(base_path / ".env.base")
+load_dotenv(base_path / "environments/.env.base")
 
 # Get the current environment, defaulting to 'development' if 'DJANGO_ENV' is not set
 ENVIRONMENT = os.environ.get("DJANGO_ENV", DEVELOPMENT)
@@ -19,11 +19,11 @@ ENVIRONMENT = os.environ.get("DJANGO_ENV", DEVELOPMENT)
 # Check if the environment is production
 if ENVIRONMENT == PRODUCTION:
     # Load environment variables from the .env.production file
-    load_dotenv(base_path / ".env.production")
+    load_dotenv(base_path / "environments/.env.production")
     # Import production-specific settings
     from .production import *
 else:
     # Load environment variables from the .env.development file
-    load_dotenv(base_path / ".env.development")
+    load_dotenv(base_path / "environments/.env.development")
     # Import development-specific settings
     from .development import *
