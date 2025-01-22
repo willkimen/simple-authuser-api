6864c5440a73f3a1b5f3bd368469dff9b213e792
Willkimen <willkimen.dev@proton.me>
2025-01-22 11:22:04 -0300

    feat: Creates specific queues for specific tasks.

--- api/config/celery.py
+++ api/config/celery.py
@@ -1,11 +1,25 @@
 import os
 
 from celery import Celery
+from user_app.constants.celery_constants import (
+    EMAIL_QUEUE_NAME,
+    REMOVE_EXPIRED_CODE_TOKEN_QUEUE_NAME,
+)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery("config")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.conf.task_routes = {
+    "user_app.tasks.task_remove_exp_code": {
+        "queue": REMOVE_EXPIRED_CODE_TOKEN_QUEUE_NAME
+    },
+    "user_app.tasks.task_remove_exp_token": {
+        "queue": REMOVE_EXPIRED_CODE_TOKEN_QUEUE_NAME
+    },
+    "user_app.tasks.": {"queue": EMAIL_QUEUE_NAME},
+}
 
 app.autodiscover_tasks()
